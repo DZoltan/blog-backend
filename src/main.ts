d0c0bf69a9d0e32d682bbd3749037233fa658faa
DZoltan <55488265+DZoltan@users.noreply.github.com>
2023-12-04 21:11:59 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const cors = require('cors');
+  const corsOptions ={
+    origin:'http://localhost:3000',
+    credentials:true,
+    optionSuccessStatus: 200
+  }
+  app.use(cors(corsOptions));
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
